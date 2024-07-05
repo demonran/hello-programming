4058e3f51ff7cffbb57b80f76687776b3a663590
ran.liu <445999306@qq.com>
2024-07-05 17:10:36 +0800

    增加LayoutLayout

--- hello-typescript/frontend/src/router/index.ts
+++ hello-typescript/frontend/src/router/index.ts
@@ -1,19 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { CssViews } from '@/constants'
 
 
-const cssRoutes = [
-  {
-    path: 'translucent-borders',
-    name: 'translucent-borders',
-    component: () => import('@/views/css/LearnCss.vue')
-  },
-  {
-    path: 'multiple-borders',
-    name: 'multiple-borders',
-    component: () => import('@/views/css/MultipleBorder.vue')
-  }
-]
+const cssRoutes: RouteRecordRaw[] = []
+
+CssViews.forEach(item => {
+  cssRoutes.push({
+    path: item.path,
+    name: item.name,
+    component: () => import(`@/views/css/${item.path}.vue`)
+  })
+})
+
 
 
 const router = createRouter({
@@ -21,34 +20,42 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Layout',
+      component: () => import('@/layout/index.vue'),
+      redirect: '/',
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView
+        },
+        {
+          path: '/about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('../views/AboutView.vue')
+        },
+        {
+          path: '/light',
+          name: 'light',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('@/views/light/LightView.vue')
+        },
+        {
+          path: '/css',
+          name: 'css',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('@/views/css/index.vue'),
+          children: cssRoutes
+        }
+      ]
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/light',
-      name: 'light',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/light/LightView.vue')
-    },
-    {
-      path: '/css',
-      name: 'css',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/css/index.vue'),
-      children: cssRoutes
-    }
   ]
 })
 
