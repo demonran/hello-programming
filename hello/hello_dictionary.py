609ab01e49047e7d1bde96989a3b2b0a72a9f5e2
ran.liu <445999306@qq.com>
2024-07-05 13:14:38 +0800

    first commit

--- hello/hello_dictionary.py
+++
@@ -1,9 +0,0 @@
-from Tix import Tk
-dict = {}
-
-dict["a"]=Tk()
-
-print dict
-
-for d in dict:
-    print d
--- hello/hello_input.py
+++
@@ -1,6 +0,0 @@
-#coding=UTF-8
-def proc():
-    str = input("请输入：")
-    print "你输入的是：",str
-
-proc()
